ef787e8c0f83d7338393db39fb167a827a72256a
crawfordpadilla <262039276+crawfordpadilla@users.noreply.github.com>
2026-01-14 23:05:41 +0000

    Added new Data and User interfaces

---
+++ tests/unit/types.ts
@@ -0,0 +1,13 @@
+export interface Data {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+}
+
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+}
